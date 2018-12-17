50e27fd038104f0c1e4bedf2b7d2a8dd5ad42797
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-12-16 23:39:07 -0300

    separar logica de cliente servidor a thread

--- src/sd/CSThread.java
+++ src/sd/CSThread.java
@@ -8,16 +8,20 @@ class CSThread implements Runnable {
 	private volatile boolean exit = false;
 	// Tipo, cliente = 0, servidor = 1
 	private int tipo = 0;
+	private int port = 9090;
+	private String ipMaquina = "";
 	Vector mensajes;
    	
-
-   	CSThread(String name) {
-      	threadName = name;
+	// Constructor para clientes con ip del servidor esperado
+   	CSThread(String name, String ip) {
+		  threadName = name;
+		  ipMaquina = ip;
 	}
-	
-	CSThread(String name, int type) {
+	// Constructor para servidores con ip en el cual se inicializa
+	CSThread(String name, int type, String ip) {
 		threadName = name;
 		tipo = type;
+		ipMaquina = ip;
 	}
 
    	// Acciones mientras corre el thread
@@ -34,12 +38,123 @@ class CSThread implements Runnable {
 			if (tipo == 0) {
 				// Si hay mensajes, actuar
 				if (!mensajes.isEmpty()) {
+					try {
+						// Asignar ip del host
+						String host = ipMaquina;
+						System.out.println("Intentando conectarse a host: "+host);
+						System.out.println("En puerto: "+port);
+						InetAddress address = InetAddress.getByName(host);
+						socket = new Socket(address, port);
+						System.out.println("Creacion exitosa del socket");
+			
+						// Try debuggear socket
+						String ip = socket.getInetAddress().getHostAddress();
+						int puerto = socket.getLocalPort();
+						System.out.println("En ip: "+ip+" y puerto: "+Integer.toString(puerto));
+			
+						// Enviar mensaje al servidor
+						OutputStream os = socket.getOutputStream();
+						OutputStreamWriter osw = new OutputStreamWriter(os);
+						BufferedWriter bw = new BufferedWriter(osw);
+			 
+						String number = "2";
+			 
+						String sendMessage = number + "\n";
+						bw.write(sendMessage);
+						bw.flush();
+						System.out.println("Msg sent to server: "+sendMessage);
+			 
+						// Recibir mensaje devuelta
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is);
+						BufferedReader br = new BufferedReader(isr);
+						String message = br.readLine();
+						System.out.println("Mensaje recibido del servidor: " +message);
+					}
+			
+					// Error al inicializar server
+					catch (Exception e) {
+						e.printStackTrace();
+						System.out.println("Error al iniciar servidor");
+						System.exit(1);
+					}
+					finally {
+						// Intentar cerrar socket
+						try {
+							socket.close();
+						}
+						catch(Exception e) {
+							e.printStackTrace();
+							System.out.println("Error al cerrar el socket");
+							System.exit(1);
+						}
+					}
 
 				}
 			}
 			// Servidor
 			else {
-
+				 // Intentar escuchar en puerto (servidor)
+				 try {
+					int port = 9090;
+					String host_ip = ipMaquina;
+					InetAddress address = InetAddress.getByName(host_ip);
+					ServerSocket serverSocket = new ServerSocket(port, 10, address);
+					System.out.println("Servidor empezado y escuchando en puerto "+port+" del ip "+host_ip);
+		 
+					// Servidor escucha siempre
+					while(!exit) {
+						// Checkear ip y puerto
+						String ip = serverSocket.getInetAddress().getHostAddress();
+						int puerto = serverSocket.getLocalPort();
+						System.out.println("En ip: "+ip+" y puerto: "+Integer.toString(puerto));
+		
+						// Esperar mensaje de cliente
+						socket = serverSocket.accept();
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is);
+						BufferedReader br = new BufferedReader(isr);
+						String number = br.readLine();
+						System.out.println("Mensaje recibido del cliente es: "+number);
+		 
+						// Tratar de multiplicar por 2 y responder de vuelta
+						String returnMessage;
+						try {
+							int numberInIntFormat = Integer.parseInt(number);
+							int returnValue = numberInIntFormat*2;
+							returnMessage = String.valueOf(returnValue) + "\n";
+						}
+						catch(NumberFormatException e) {
+							// Entrada no era numero
+							returnMessage = "Por favor, envie un numero adecuadamente\n";
+						}
+		 
+						// Enviando respuesta devuelta
+						OutputStream os = socket.getOutputStream();
+						OutputStreamWriter osw = new OutputStreamWriter(os);
+						BufferedWriter bw = new BufferedWriter(osw);
+						bw.write(returnMessage);
+						System.out.println("Mensaje enviado al cliente es: "+returnMessage);
+						bw.flush();
+					}
+				}
+				// Error al inicializar server
+				catch (Exception e) {
+					e.printStackTrace();
+					System.out.println("Error al iniciar servidor");
+					System.exit(1);
+				}
+		
+				finally {
+					// Intentar cerrar socket
+					try {
+						socket.close();
+					}
+					catch(Exception e){
+						System.out.println("Error al cerrar el socket");
+						System.exit(1);
+					}
+				}
 			}
 		}
 	}
--- src/sd/Hospital.java
+++ src/sd/Hospital.java
@@ -24,148 +24,39 @@ public class Hospital {
     private static Socket socket;
     public static void main (String[] args) {
         int nMaquina = 0;
+        String ipMaquina = "";
         for (String s: args) {
             if (s.equals("37")) {
                 System.out.println(ip37);
                 nMaquina = 37;
+                ipMaquina = ip37;
             }
             if (s.equals("38")) {
                 System.out.println(ip38);
                 nMaquina = 38;
+                ipMaquina = ip38;
             }
             if (s.equals("39")) {
                 System.out.println(ip39);
                 nMaquina = 39;
+                ipMaquina = ip39;
             }
             if (s.equals("40")) {
                 System.out.println(ip40);
                 nMaquina = 40;
+                ipMaquina = ip40;
             }
         }
         System.out.println("Run exitoso");
 
         // Si es maquina 38 => servidor
-        if (nMaquina == 38) {
-        // Intentar escuchar en puerto
-        try {
-            int port = 9090;
-            //ServerSocket serverSocket = new ServerSocket(port);
-            String host_ip = ip38;
-            InetAddress address = InetAddress.getByName(host_ip);
-            ServerSocket serverSocket = new ServerSocket(port, 10, address);
-            System.out.println("Servidor empezado y escuchando en puerto "+port+" del ip "+host_ip);
- 
-            // Servidor escucha siempre
-            while(true) {
-                // Checkear ip y puerto
-                String ip = serverSocket.getInetAddress().getHostAddress();
-                int puerto = serverSocket.getLocalPort();
-                System.out.println("En ip: "+ip+" y puerto: "+Integer.toString(puerto));
+        ///if (nMaquina == 38) {
 
-                // Esperar mensaje de cliente
-                socket = serverSocket.accept();
-                InputStream is = socket.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String number = br.readLine();
-                System.out.println("Mensaje recibido del cliente es: "+number);
- 
-                // Tratar de multiplicar por 2 y responder de vuelta
-                String returnMessage;
-                try {
-                    int numberInIntFormat = Integer.parseInt(number);
-                    int returnValue = numberInIntFormat*2;
-                    returnMessage = String.valueOf(returnValue) + "\n";
-                }
-                catch(NumberFormatException e) {
-                    // Entrada no era numero
-                    returnMessage = "Por favor, envie un numero adecuadamente\n";
-                }
- 
-                // Enviando respuesta devuelta
-                OutputStream os = socket.getOutputStream();
-                OutputStreamWriter osw = new OutputStreamWriter(os);
-                BufferedWriter bw = new BufferedWriter(osw);
-                bw.write(returnMessage);
-                System.out.println("Mensaje enviado al cliente es: "+returnMessage);
-                bw.flush();
-            }
-        }
-        // Error al inicializar server
-        catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error al iniciar servidor");
-            System.exit(1);
-        }
-
-        finally {
-            // Intentar cerrar socket
-            try {
-                socket.close();
-            }
-            catch(Exception e){
-                System.out.println("Error al cerrar el socket");
-                System.exit(1);
-            }
-        }
-
-        }
+        //}
         
         // Si maquina no era numero 38 => cliente
-        else {
-            try {
-            // Asignar ip del host
-            String host = ip38;
-            System.out.println("Intentando conectarse a host: "+host);
-            int port = 9090;
-            System.out.println("En puerto: "+port);
-            InetAddress address = InetAddress.getByName(host);
-            socket = new Socket(address, port);
-            System.out.println("Creacion exitosa del socket");
+        //else {
 
-            // Try debuggear socket
-            String ip = socket.getInetAddress().getHostAddress();
-            int puerto = socket.getLocalPort();
-            System.out.println("En ip: "+ip+" y puerto: "+Integer.toString(puerto));
-
-            // Enviar mensaje al servidor
-            OutputStream os = socket.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os);
-            BufferedWriter bw = new BufferedWriter(osw);
- 
-            String number = "2";
- 
-            String sendMessage = number + "\n";
-            bw.write(sendMessage);
-            bw.flush();
-            System.out.println("Msg sent to server: "+sendMessage);
- 
-            // Recibir mensaje devuelta
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String message = br.readLine();
-            System.out.println("Mensaje recibido del servidor: " +message);
-        }
-
-        // Error al inicializar server
-        catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error al iniciar servidor");
-            System.exit(1);
-        }
-        finally {
-            // Intentar cerrar socket
-            try {
-                socket.close();
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-                System.out.println("Error al cerrar el socket");
-                System.exit(1);
-            }
-        }
-
-        }
+        //}
     }
 }
