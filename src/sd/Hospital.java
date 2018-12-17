abbe188334c6a369828c4fd9ffdcb9bbe9b2ec46
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-12-17 07:36:56 -0300

    actualizados archivos de personal

--- src/sd/Hospital.java
+++ src/sd/Hospital.java
@@ -4,6 +4,7 @@ import com.google.gson.*;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
+import java.lang.*;
 
 public class Hospital {
     // Ips asociados a maquinas virtuales
@@ -31,22 +32,57 @@ public class Hospital {
         }
         return max;
     }
-    public static void main (String[] args) throws InterruptedException {
-        // Lectura inicial de json
-        URL path = Hospital.class.getResource("../Doctores1.JSON");
-        File f = new File(path.getFile());
-        /*catch(FileNotFoundException e) {
-            System.out.println("Archivo no encontrado");
-            System.exit(1);
-        }*/
-        Map javaRootMapObject = null;
-        try {
-            javaRootMapObject = new Gson().fromJson(new FileReader(f), Map.class);
+    public static void main (String[] args) throws InterruptedException, FileNotFoundException {
+        // Inicializacion de variables relativas por maquina
+        int nMaquina = 0;
+        String ipMaquina = "";
+        String cliente1 = "";
+        String cliente2 = "";
+        String cliente3 = "";
+        String nombreArchivoPersonal = "";
+        // Identificar maquina actual
+        for (String s: args) {
+            if (s.equals("37")) {
+                System.out.println(ip37);
+                nMaquina = 37;
+                ipMaquina = ip37;
+                cliente1 = ip38;
+                cliente2 = ip39;
+                cliente3 = ip40;
+                nombreArchivoDoctores = "Personal37.json";
+            }
+            if (s.equals("38")) {
+                System.out.println(ip38);
+                nMaquina = 38;
+                ipMaquina = ip38;
+                cliente1 = ip37;
+                cliente2 = ip39;
+                cliente3 = ip40;
+                nombreArchivoDoctores = "Personal38.json";
+            }
+            if (s.equals("39")) {
+                System.out.println(ip39);
+                nMaquina = 39;
+                ipMaquina = ip39;
+                cliente1 = ip37;
+                cliente2 = ip38;
+                cliente3 = ip40;
+                nombreArchivoDoctores = "Personal39.json";
+            }
+            if (s.equals("40")) {
+                System.out.println(ip40);
+                nMaquina = 40;
+                ipMaquina = ip40;
+                cliente1 = ip37;
+                cliente2 = ip38;
+                cliente3 = ip39;
+                nombreArchivoDoctores = "Personal40.json";
+            }
         }
-        catch(FileNotFoundException e) {
-            System.out.println("Archivo no encontrado");
-            System.exit(1);
-        }
+
+        // Lectura inicial de json 
+        File f = new File(nombreArchivoPersonal);
+        Map javaRootMapObject = new Gson().fromJson(new FileReader(f), Map.class);
         // Formular personal hospital
         List doctores = (List) javaRootMapObject.get("Doctor");
         List paramedicos = (List) javaRootMapObject.get("Paramedico");
@@ -82,58 +118,9 @@ public class Hospital {
         System.out.println("Fin!"); //punto para debug
 
 
-        // Inicializacion de clientes - servidor para comunicacion maquina
-        int nMaquina = 0;
-        String ipMaquina = "";
-        String cliente1 = "";
-        String cliente2 = "";
-        String cliente3 = "";
-        // Identificar maquina actual
-        for (String s: args) {
-            if (s.equals("37")) {
-                System.out.println(ip37);
-                nMaquina = 37;
-                ipMaquina = ip37;
-                cliente1 = ip38;
-                cliente2 = ip39;
-                cliente3 = ip40;
-            }
-            if (s.equals("38")) {
-                System.out.println(ip38);
-                nMaquina = 38;
-                ipMaquina = ip38;
-                cliente1 = ip37;
-                cliente2 = ip39;
-                cliente3 = ip40;
-            }
-            if (s.equals("39")) {
-                System.out.println(ip39);
-                nMaquina = 39;
-                ipMaquina = ip39;
-                cliente1 = ip37;
-                cliente2 = ip38;
-                cliente3 = ip40;
-            }
-            if (s.equals("40")) {
-                System.out.println(ip40);
-                nMaquina = 40;
-                ipMaquina = ip40;
-                cliente1 = ip37;
-                cliente2 = ip38;
-                cliente3 = ip39;
-            }
-        }
-        System.out.println("Run exitoso");
-
-        // Si es maquina 38 => servidor
-        ///if (nMaquina == 38) {
-
-        //}
         
-        // Si maquina no era numero 38 => cliente
-        //else {
 
-        //}
+        // Inicializacion de servidor y clientes
         CSThread servidor = new CSThread("server", 1, ipMaquina);
         servidor.start();
         // Esperar inicializacion de otros servidores antes de ejecutar clientes
